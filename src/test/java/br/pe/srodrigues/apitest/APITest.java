de6eea95404e9b1143289884e089bc291253f153
Rodrigues, Sandro <sandro.rodrigues@accenture.com>
2021-02-10 18:58:21 -0300

    first commit

---
+++ src/test/java/br/pe/srodrigues/apitest/APITest.java
@@ -0,0 +1,56 @@
+package br.pe.srodrigues.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+
+	//Método estático por ser executado apenas uma única vez.
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200);
+	}
+	
+	@Test
+	public void deveAdicionarTarefaComSucesso() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste Via API\", \"dueDate\": \"2021-02-09\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201);
+	}
+	
+	@Test
+	public void naoDeveAdicionarTarefaInvalida() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste Via API\", \"dueDate\": \"2020-02-09\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"));
+	}
+
+}
+
+
+
+	
+
